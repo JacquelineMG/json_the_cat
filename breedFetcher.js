b1e38f78155b4f157e8549fc8b7654631e09d28c
JacquelineMG <j.gilchrist@outlook.com>
2023-05-18 14:19:45 -0700

    refactor breedFetcher and add testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,38 @@
 const request = require('request');
 
-const breed = process.argv[2];
-const url = "http://api.thecatapi.com/v1/breeds/search?q=" + breed;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
+const fetchBreedDescription = function(breedName, callback) {
+  const url = "http://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
     const data = JSON.parse(body);
     const info = data[0];
-    if (info === undefined) {
-      console.log(`ERROR: ${breed} can't be found`);
+    if (info) {
+      callback(null, info.description);
     } else {
-      const description = info.description;
-      console.log(description);
+      callback(`ERROR: ${breedName} can't be found`, null);
+    }
+  });
+};
+    
+
+
+module.exports = { fetchBreedDescription };
+
+/*
+const fetchBreedDescription = function(breedName, callback) {
+  request(url+breedName, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const data = JSON.parse(body);
+      const info = data[0];
+      if (info === undefined) {
+      console.log(`ERROR: ${breedName} can't be found`);
+    } else {
+      const desc = info.description;
+      console.log(desc);
     }
   }
-});
+})}; */
